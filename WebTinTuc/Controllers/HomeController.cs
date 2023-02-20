b8b2490d1fcffa135940c37d5b9d0b4c43eccc6d
VuTrongHanh99 <vuhanhcntt899@gmail.com>
2023-02-20 17:09:08 +0700

    Mau 1 chua service

---
+++ WebTinTuc/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebTinTuc.BackEnd.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
